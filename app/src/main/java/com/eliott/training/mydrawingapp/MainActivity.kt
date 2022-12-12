dc9249bd1f5d13f2a1fa44b2de17f8265abc82b4
PC-EJOUANJAN\ejouanjan <eliott6@live.fr>
2022-12-12 17:58:02 +0100

    Add permissions

--- app/src/main/java/com/eliott/training/mydrawingapp/MainActivity.kt
+++ app/src/main/java/com/eliott/training/mydrawingapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.eliott.training.mydrawingapp
 
+import android.Manifest
 import android.app.Dialog
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
@@ -7,7 +8,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -15,6 +21,29 @@ class MainActivity : AppCompatActivity() {
 
     private var mImageButtonCurrentPaint: ImageButton? = null
     private var drawingView: DrawingView? = null
+
+    val requestPermissions: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val perMissionName = it.key
+                val isGranted = it.value
+                if (isGranted) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Permission granted now you can read the storage files.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    if (perMissionName == Manifest.permission.READ_EXTERNAL_STORAGE)
+                        Toast.makeText(
+                            this@MainActivity ,
+                            "Oops you just denied the permission.",
+                            Toast.LENGTH_LONG
+                        ).show()
+                }
+            }
+
+        }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,9 +61,14 @@ class MainActivity : AppCompatActivity() {
         )
 
 
-        var ib_brush: ImageButton = findViewById(R.id.ib_brush)
+        val ib_brush: ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener {
             showBrushSizeChooserDialog()
+        }
+
+        val ib_gallery: ImageButton = findViewById(R.id.ib_gallery)
+        ib_gallery.setOnClickListener {
+            requestStoragePermission()
         }
 
     }
@@ -81,5 +115,38 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    }
+
+    private fun requestStoragePermission(){
+        if (
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+        ){
+            showRationaleDialog("Drawing App","Drawing App " +
+                    "needs to Access Your External Storage")
+        }
+        else {
+            requestPermissions.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            )
+        }
+
+    }
+
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
     }
 }
